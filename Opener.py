ad307f7455e06c32b75704edfb582d744daca5fe
Nicola Panozzo <nicolapanoz@gmail.com>
2019-10-14 15:50:38 +0200

    modify favoriteList as json

--- Opener.py
+++ Opener.py
@@ -3,32 +3,29 @@
 #FastWebOpener 1.0
 
 import os
-
+import json
 
 
 pathToApplications =  "/Applications"
 
 def getLinks():
 
-	
-	
-	favoriteList_file = open('favoriteList', 'r')
-	bookmarks = favoriteList_file.readlines()
-
-	for bookmark in bookmarks:
-		print(bookmark)
-	return bookmarks
+	links = [];
+	with open('favoriteList') as json_file:
+		data = json.load(json_file)
+		for p in data['keywords']:
+			links.append(p)
+		return links
 
 
 def openLinks():
 	
 	linksString = ""
 	for link in links:
-		linksString = linksString + "'%s' " % (link)
+		linksString = linksString + " %s" % (link)
 		
 	os.system("cd /Applications && open Google\ Chrome.app/ --args --new-window -a  %s" % (linksString))
 
 links = getLinks()
 openLinks()
-
 
